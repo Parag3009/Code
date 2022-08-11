872c7d7f3c3e18bf0ef6841658fe759458f6cc9b
Parag3009 <109575138+Parag3009@users.noreply.github.com>
2022-08-11 09:59:46 +0530

    Add files via upload

---
+++ sum1.java
@@ -0,0 +1,10 @@
+public class sum1
+{  
+public static void main(String args[])  
+{  
+int x = 5; //first arguments   
+int y = 5; //second arguments   
+int sum = x + y;  
+System.out.println("The sum of x and y is: " +sum);  
+}  
+}  
